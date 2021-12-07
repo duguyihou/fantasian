608459106ea9bb5287c30240477cece3306b174f
kong <duguyihou@gmail.com>
2021-12-07 21:15:43 +1100

    chore: update

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { User } from './schema/user.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
